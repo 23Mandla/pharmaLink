789838fa1e2a5e5e6f5977c914d7ceb0775af5d6
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-04 23:52:16 +0200

    New feature(Login.kt): added login activity

---
+++ app/src/main/java/com/example/pharmalink/feature/home/MainPage.kt
@@ -0,0 +1,19 @@
+package com.example.pharmalink.feature.home
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainPage(){
+    LazyColumn (
+        modifier = Modifier
+            .fillMaxSize()
+
+    ) {
+        modifier = Modifier
+            .fillMaxSize()
+
+    }
+}
---
+++ app/src/main/java/com/example/pharmalink/feature/login/Login.kt
@@ -0,0 +1,179 @@
+package com.example.pharmalink.feature.login
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.*
+import com.example.pharmalink.R
+
+@Composable
+@Preview
+fun Login(onClick: () -> Unit = {}){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+    var emailError by remember { mutableStateOf("") }
+    var passwordError by remember { mutableStateOf("") }
+
+    //TODO Move column and Row inside a box and components
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Login",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = { Text(
+                email.ifEmpty { "Email" },
+                color = if (emailError.isNotEmpty()) Color.Red else Color.Unspecified
+            ) },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(R.drawable.baseline_email_24),
+                    contentDescription = "Email Icon"
+                )
+            },
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 23.dp),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = { Text(
+                email.ifEmpty { "Password" },
+                color = if (passwordError.isNotEmpty()) Color.Red else Color.Unspecified
+            ) },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(R.drawable.baseline_lock_24),
+                    contentDescription = "Password Icon"
+                )
+            },
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image = if (passwordVisible)
+                    painterResource(R.drawable.baseline_visibility_24)
+                else
+                    painterResource(R.drawable.baseline_visibility_off_24)
+                Icon(
+                    painter = image,
+                    contentDescription = "Password Toggle",
+                    modifier = Modifier.clickable {passwordVisible = !passwordVisible}
+                )
+
+            },
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 23.dp),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 23.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Remember Me",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold
+
+            )
+
+            Text(
+                text = "Forgot Password?",
+                color = Color.Blue,
+                modifier = Modifier.clickable{
+                    //TODO forgot password logic
+                },
+                fontSize = 14.sp,
+
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                emailError = if (email.isEmpty()) "Email is required" else ""
+                passwordError = if (password.isEmpty()) "Password is required" else ""
+                if(emailError.isEmpty() && passwordError.isEmpty()){
+                    //TODO Login logic
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 23.dp),
+            shape = RoundedCornerShape(8.dp),
+        ) {
+            Text(
+                text = "Login",
+                fontSize = 16.sp,
+
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row {
+            Text(text = "Don't have an account ?")
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Sign Up",
+                modifier = Modifier.clickable {
+                    //TODO handle sign up logic
+                },
+                color = Color.Blue
+            )
+
+        }
+
+    }
+
+}

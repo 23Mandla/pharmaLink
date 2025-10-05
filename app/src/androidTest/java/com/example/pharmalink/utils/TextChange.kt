1cdaf38b6af0132cbf3f729e3a64687b905871f3
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-05 18:27:32 +0200

    New feature: Testing login UI change

---
+++ app/src/androidTest/java/com/example/pharmalink/utils/TextChange.kt
@@ -0,0 +1,50 @@
+package com.example.pharmalink.utils
+
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertTextContains
+import androidx.compose.ui.test.junit4.ComposeTestRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+
+fun TextChange(
+    tag: String,
+    textChange: String,
+    rule: ComposeTestRule
+) : Unit {
+
+    if (tag == "emailField") {
+        rule.onNodeWithTag(
+            tag
+        )
+            .assertExists()
+            .performClick()
+            .performTextInput(textChange)
+
+        rule.onNodeWithTag(
+            tag
+        )
+            .assertTextContains(textChange)
+
+        rule.onNodeWithTag(
+            "loginButton"
+        )
+            .assertIsEnabled()
+    } else if (tag == "passwordField") {
+        rule.onNodeWithTag(
+            tag
+        )
+            .assertExists()
+            .performClick()
+            .performTextInput(textChange)
+
+        rule.onNodeWithTag(
+            tag
+        )
+            .assertTextContains(textChange)
+
+        rule.onNodeWithTag(
+            "loginButton"
+        )
+    }
+}

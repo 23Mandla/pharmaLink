3bda9bd7dcdb7f88bd00f274fbe278c96bbbbbe6
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-05 18:32:32 +0200

    Updating versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.ui.test.junit4)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -58,4 +59,6 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     // splash screen
     implementation(libs.androidx.core.splashscreen)
+    // navigation
+    implementation(libs.androidx.navigation.compose)
 }

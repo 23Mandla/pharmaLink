9b5fa5dac2c5aad19933f5d54da1a546fb94844c
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-05 18:22:52 +0200

    Update imported AppNav composable

--- app/src/main/java/com/example/pharmalink/MainActivity.kt
+++ app/src/main/java/com/example/pharmalink/MainActivity.kt
@@ -3,15 +3,9 @@ package com.example.pharmalink
 import android.os.*
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.pharmalink.navigation.AppNav
 import com.example.pharmalink.ui.theme.PharmaLinkTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,29 +21,8 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             PharmaLinkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNav()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PharmaLinkTheme {
-        Greeting("Android")
     }
 }

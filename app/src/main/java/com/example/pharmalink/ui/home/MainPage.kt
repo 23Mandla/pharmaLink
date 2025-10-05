f22bef36ee7585929c4a07c553e34312dba62f33
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-05 18:24:07 +0200

    Update: Updating main page with sections

--- app/src/main/java/com/example/pharmalink/ui/home/MainPage.kt
+++ app/src/main/java/com/example/pharmalink/ui/home/MainPage.kt
@@ -1,17 +1,24 @@
-package com.example.pharmalink.feature.home
+package com.example.pharmalink.ui.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pharmalink.ui.components.HeaderSection
 
 @Composable
+@Preview
 fun MainPage(){
     LazyColumn (
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.White)
 
     ) {
+        item { HeaderSection() }
 
     }
 }

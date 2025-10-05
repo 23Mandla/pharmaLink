c7ec3b9f8e6869a8ca72b7b82f0b623b529dc8eb
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-05 18:28:29 +0200

    New feature: Added a file for app navigation

---
+++ app/src/main/java/com/example/pharmalink/navigation/AppNav.kt
@@ -0,0 +1,50 @@
+package com.example.pharmalink.navigation
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key.Companion.Home
+import androidx.compose.ui.res.colorResource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.pharmalink.R
+import com.example.pharmalink.ui.home.MainPage
+import com.example.pharmalink.ui.login.Login
+
+@Composable
+fun AppNav(){
+    val navController = rememberNavController()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = backStackEntry?.destination?.route
+
+    Scaffold(
+        containerColor = colorResource(R.color.white),
+        contentWindowInsets = WindowInsets(0, 0, 0, 0)
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+
+        ){
+            NavHost(
+                navController = navController,
+                startDestination = "login",
+                modifier = Modifier.fillMaxSize()
+            ) {
+                composable("login") {
+                    Login(onClick = { navController.navigate("mainPage") })
+                }
+                composable("mainPage") {
+                    MainPage()
+                }
+            }
+        }
+
+    }
+
+}

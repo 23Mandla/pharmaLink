6aa5ed59de6c86ef181979a15d102e4e5da11977
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-07 03:43:40 +0200

    Update: updating UI header

--- app/src/main/java/com/example/pharmalink/ui/components/HeaderSection.kt
+++ app/src/main/java/com/example/pharmalink/ui/components/HeaderSection.kt
@@ -1,27 +1,72 @@
 package com.example.pharmalink.ui.components
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
+import com.example.pharmalink.R
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
-fun HeaderSection(){
-    Row (modifier = Modifier
-        .padding(
-            horizontal = 16.dp,
-            vertical = 24.dp
+fun HeaderSection(topBar: @Composable () -> Unit = {}){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .padding(10.dp),
+
+        colors = CardDefaults.cardColors(
+            containerColor = colorResource(R.color.lightNavy)
+        ),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
         )
-        .fillMaxWidth()
-        .statusBarsPadding(),
-        verticalAlignment = Alignment.CenterVertically
     ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp, bottom = 30.dp, top = 12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
 
-        Text("Welcome back Mandla")
+        ) {
+            Text(
+                "Today",
+                fontSize = 20.sp,
+                color = Color.White
+            )
 
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                today(),
+                fontSize = 16.sp,
+                color = Color.White
+            )
+        }
     }
+}
+
+// return tody's date
+@RequiresApi(Build.VERSION_CODES.O)
+fun today(): String {
+    val today = LocalDate.now()
+    val date = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+
+    return today.format(date)
+
 }

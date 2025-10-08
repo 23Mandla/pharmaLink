30fbe4d93421ae72ac5e734e4c4aedd558eb53eb
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-08 10:56:35 +0200

    New feature: header section for mediation filter

---
+++ app/src/main/java/com/example/pharmalink/ui/components/MedsHeader.kt
@@ -0,0 +1,47 @@
+package com.example.pharmalink.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.*
+import com.example.pharmalink.R
+
+@Composable
+@Preview
+fun MedsHeader(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .padding(16.dp)
+            .background(Color.White),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+
+    ) {
+        Text(
+            "Medications",
+            fontSize = 20.sp,
+
+        )
+
+        IconButton (
+            onClick = { /*TODO add filter logic**/ } ,
+            modifier = Modifier
+                .testTag("filterToggle")
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.filter),
+                contentDescription = "filter",
+                modifier = Modifier.size(20.dp)
+            )
+        }
+    }
+}

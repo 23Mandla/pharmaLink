27ac03ac9b45ab46bcdb315efb98cb57e6edd12a
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-11 10:26:55 +0200

    Update: working on medication page

--- app/src/main/java/com/example/pharmalink/ui/medication/MedicationPage.kt
+++ app/src/main/java/com/example/pharmalink/ui/medication/MedicationPage.kt
@@ -1,15 +1,34 @@
 package com.example.pharmalink.ui.medication
 
+import android.content.res.Resources
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pharmalink.R
 
 @Composable
-fun MedicationPage(){
-    Box(
+@Preview
+fun MedicationPage(onSendMessage : (String) -> Unit = {}){
+    var message by remember {
+        mutableStateOf("")
+    }
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
@@ -18,13 +37,45 @@ fun MedicationPage(){
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .verticalScroll(rememberScrollState()),
+                .verticalScroll(rememberScrollState())
+                .background(colorResource(R.color.lighterNavy)),
             horizontalArrangement = Arrangement.Center
         ) {
             Text(
                 text = "Medication Page",
-                color = Color.Black
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(16.dp),
+                maxLines = 1,
+                fontSize = 22.sp
             )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value = message,
+                onValueChange = {
+                    message = it
+                },
+                label = { Text("Search") }
+            )
+
+            IconButton(
+                onClick = {
+                    onSendMessage(message)
+                    message = ""
+                }
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.baseline_send_24),
+                    contentDescription = "Search"
+                )
+            }
         }
     }
 }

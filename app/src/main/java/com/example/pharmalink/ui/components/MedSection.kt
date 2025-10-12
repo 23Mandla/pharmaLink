42432b033be4f8df7cb1580654de20ed4d85967f
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-13 00:37:48 +0200

    Updated: Medication section to prioritize medication

--- app/src/main/java/com/example/pharmalink/ui/components/MedSection.kt
+++ app/src/main/java/com/example/pharmalink/ui/components/MedSection.kt
@@ -1,17 +1,27 @@
 package com.example.pharmalink.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.grid.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
 import androidx.navigation.NavHostController
 import com.example.pharmalink.data.dataclass.Medication
 
 @Composable
+@Preview
 fun MedSection(
     medications: List<Medication> = emptyList(),
     navController: NavHostController? = null,
@@ -19,28 +29,69 @@ fun MedSection(
 ){
     val context = LocalContext.current
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2), // Adaptive
-        contentPadding = PaddingValues(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(10.dp),
-        verticalArrangement = Arrangement.spacedBy(10.dp),
+    Column (
+
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.White)
-            .height(500.dp)
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+
     ) {
 
-        items(medications) { medication ->
-            MedCard(
-                context = context,
-                medication = medication,
-                onClick = {
-                    if (navController != null) {
-                        navController.navigate("medicationPage")
-                        onMedClick(medication)
-                    }
+        //TODO NB in dev
+
+       Row(
+           modifier = Modifier
+               .fillMaxWidth()
+               .padding(vertical = 10.dp),
+           horizontalArrangement = Arrangement.SpaceEvenly,
+
+       ) {
+           repeat(3){
+               //TODO get med card over here
+               Card(
+                   modifier = Modifier
+                       .size(90.dp)
+                       .clickable {
+                          //TODO med card is clickable
+                       },
+                   colors = CardDefaults.cardColors(
+                       containerColor = Color.White
+                   ),
+                   border = CardDefaults.outlinedCardBorder(),
+               ) {
+
+               }
+           }
+
+       }
+
+        Spacer(modifier = Modifier.height(2.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+
+            ) {
+            repeat(3){
+                //TODO get med card over here
+                Card(
+                    modifier = Modifier
+                        .size(95.dp)
+                        .clickable {
+                            //TODO med card is clickable
+                        },
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.White
+                    ),
+                    border = CardDefaults.outlinedCardBorder(),
+                ) {
+
                 }
-            )
+            }
         }
     }
 }

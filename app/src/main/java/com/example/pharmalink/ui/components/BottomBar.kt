ea989f4b4ccec49d1ad06eef6132db4f53eecc1b
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-28 06:35:25 +0200

    Update: updated bottom bar navigation UI

--- app/src/main/java/com/example/pharmalink/ui/components/BottomBar.kt
+++ app/src/main/java/com/example/pharmalink/ui/components/BottomBar.kt
@@ -1,5 +1,8 @@
 package com.example.pharmalink.ui.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -11,9 +14,11 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pharmalink.R
 import com.example.pharmalink.utils.bottomNav.NavItems
 
@@ -46,6 +51,9 @@ fun BottomBar(){
     }
 
     NavigationBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(63.dp),
         containerColor = MaterialTheme.colorScheme.onSurfaceVariant.copy(0.4f)
     ) {
        navItems.forEachIndexed {

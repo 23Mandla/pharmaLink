08a00778ca24c4775be73c7c6c787678777c3420
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-10-31 23:17:56 +0200

    Update: weekly card and section UI updated

--- app/src/main/java/com/example/pharmalink/ui/components/WeekDayCard.kt
+++ app/src/main/java/com/example/pharmalink/ui/components/WeekDayCard.kt
@@ -2,12 +2,16 @@ package com.example.pharmalink.ui.components
 
 import android.text.format.DateUtils.isToday
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,42 +34,75 @@ import com.example.pharmalink.R
 @Composable
 @Preview
 fun WeekDayCard(day: String = "Mon",date: String = "12", isToday: Boolean = false){
-    Card(
+
+    val backgroundColor = if (isToday) {
+        MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+    } else {
+        Color.White.copy(alpha = 0.30f)
+    }
+
+    val contentColor = if (isToday) {
+        Color.White
+    } else {
+        Color.Black
+    }
+
+    Box(
         modifier = Modifier
             .size(60.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .border(1.dp, Color.LightGray, RoundedCornerShape(16.dp))
 
     ){
-        Column(
+        Box(
             modifier = Modifier
-                .fillMaxSize()
+                .matchParentSize()
+                .clip(RoundedCornerShape(16.dp))
                 .background(
-                    if (isToday) colorResource(R.color.seafoamBlue)
-                    else
-                        colorResource(R.color.lightGrey)
-
-                ),
-            horizontalAlignment = Alignment.CenterHorizontally,
-
-        ) {
-
-            Text(
-                day,
-                color = if (isToday) Color.White else Color.Black,
-                modifier = Modifier.padding(top = 8.dp),
-
-                fontSize = 13.sp,
-                fontWeight = MaterialTheme.typography.titleMedium.fontWeight
-            )
-
-            Spacer(modifier = Modifier.height(1.dp))
-
-            Text(
-                date,
-                color = if (isToday) Color.White else Color.Black,
-                fontSize = 13.sp,
-                modifier = Modifier
+                    backgroundColor
+                )
 
             )
-        }
+
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .clip(RoundedCornerShape(16.dp))
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(Color.White.copy(alpha = 0.1f), Color.White.copy(alpha = 0.04f)),
+                        start = Offset(0f, 0f),
+                        end = Offset(1000f, 1000f)
+                    )
+                )
+                .blur(radius = 16.dp),
+
+        )
+
+           Column(
+               modifier = Modifier
+                   .fillMaxSize(),
+               horizontalAlignment = Alignment.CenterHorizontally,
+
+           ) {
+
+
+               Spacer(modifier = Modifier.height(2.dp))
+
+               Text(
+                   date,
+                   color = contentColor,
+                   fontSize = 13.sp,
+
+               )
+
+               Text(
+                   day,
+                   color = contentColor,
+                   fontSize = 13.sp,
+                   fontWeight = MaterialTheme.typography.titleMedium.fontWeight
+               )
+
+           }
     }
 }
--- app/src/main/java/com/example/pharmalink/ui/components/weekelyCalendar.kt
+++ app/src/main/java/com/example/pharmalink/ui/components/weekelyCalendar.kt
@@ -3,6 +3,7 @@ package com.example.pharmalink.ui.components
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,6 +11,8 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import java.time.DayOfWeek
@@ -43,19 +46,14 @@ fun WeeklyCalendar() {
             daysOfTheWeek
                 .take(7)
         ) { (day, date) ->
-            if (todayDay == date)
+            val isToday = todayDay == date
+
                 WeekDayCard(
                     day = day,
                     date = date,
-                    isToday = true
+                    isToday = isToday
                 )
-            else {
-                WeekDayCard(
-                    day = day,
-                    date = date,
-                    isToday = false
-                )
-            }
+
         }
     }
 }

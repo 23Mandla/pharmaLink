0f11d17cbd871fd314218744fbeef89b8cbaf225
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-11-13 05:52:24 +0200

    update: updated medication page

--- app/src/main/java/com/example/pharmalink/ui/components/medication_page/MedicationSchedule.kt
+++ app/src/main/java/com/example/pharmalink/ui/components/medication_page/MedicationSchedule.kt
@@ -1,15 +1,19 @@
 package com.example.pharmalink.ui.components.medication_page
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pharmalink.R
@@ -25,32 +30,106 @@ import com.example.pharmalink.R
 @Preview
 fun MedicationSchedule(time: String = "10:00 AM"){
     // another repetition
-    Card(
+    Row(
         modifier = Modifier
-            .height(30.dp)
-            .width(65.dp),
-        shape = RoundedCornerShape(5.dp)
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(horizontal = 13.dp, vertical = 5.dp),
+        verticalAlignment = Alignment.CenterVertically
 
-    ){
-        Row(
+
+    ) {
+        TimeLine(
             modifier = Modifier
-                .fillMaxSize()
-                .background(
+                .weight(1f)
+                .height(2.dp),
+            color = Color.Gray
+        )
 
-                        colorResource(R.color.lightGrey)
+        MedicationScheduleControl(
+            size = 20.dp,
+            color = colorResource(R.color.lightGrey),
+            modifier = Modifier,
+            time = "10:00"
+        )
 
-                ),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
+        TimeLine(
+            modifier = Modifier
+                .weight(1f)
+                .height(2.dp),
+            color = Color.Gray
+        )
 
-            ) {
+        MedicationScheduleControl(
+            size = 20.dp,
+            color = colorResource(R.color.lightGrey),
+            modifier = Modifier,
+            time = "14:00"
+        )
 
-            Text(
-                time,
-                color = Color.Black,
-                fontSize = 10.sp,
-                fontWeight = MaterialTheme.typography.titleMedium.fontWeight
-            )
-        }
+        TimeLine(
+            modifier = Modifier
+                .weight(1f)
+                .height(2.dp),
+            color = Color.Gray
+        )
+
+        MedicationScheduleControl(
+            size = 20.dp,
+            color = colorResource(R.color.lightGrey),
+            modifier = Modifier,
+            time = "18:00"
+        )
+
+        TimeLine(
+            modifier = Modifier
+                .weight(1f)
+                .height(2.dp),
+            color = Color.Gray
+        )
     }
+
+}
+
+@Composable
+fun MedicationScheduleControl(
+    size : Dp,
+    color: Color,
+    onClick: () -> Unit = {},
+    modifier: Modifier,
+    time: String = ""
+){
+    Column(
+
+    ) {
+
+        Surface(
+            shape = CircleShape,
+            border = BorderStroke(1.dp, Color.Black),
+            color = color,
+            modifier = modifier
+                .size(size)
+                .clickable { onClick() }
+        ) {
+
+        }
+
+    }
+
+}
+
+@Composable
+fun TimeLine(
+    modifier: Modifier,
+    color: Color
+){
+
+    Box(
+        modifier = modifier
+            .height(2.dp)
+            .background(color)
+
+
+    )
+
 }

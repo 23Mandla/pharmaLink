5b3d0d72c9655edaa8e130613e48b2ef623e9780
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-11-16 21:14:13 +0200

    added new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,7 @@ dependencies {
     // Add the dependency for the Firebase AI Logic library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation(libs.firebase.ai)
+
+    // pager dependency
+    implementation(libs.accompanist.pager)
 }

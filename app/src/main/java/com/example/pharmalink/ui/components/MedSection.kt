8eadf7773538ce63ae7aeb03d8c25bb40899e241
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-11-22 23:46:31 +0200

    Updated med section to recieve medication from api call

--- app/src/main/java/com/example/pharmalink/ui/components/MedSection.kt
+++ app/src/main/java/com/example/pharmalink/ui/components/MedSection.kt
@@ -1,53 +1,53 @@
 package com.example.pharmalink.ui.components
 
-import android.net.Uri
-import androidx.compose.foundation.Image
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
+import androidx.compose.ui.*
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
 import androidx.navigation.NavHostController
 import com.example.pharmalink.data.dataclass.Medication
-import com.google.gson.Gson
 import com.example.pharmalink.R
+import com.example.pharmalink.data.viewmodel.MedicationState
 import com.example.pharmalink.utils.HorizontalDivider
 
 @Composable
-@Preview
+
 fun MedSection(
-    sideEffects: List<String> = emptyList(),
+    medicationState: MedicationState,
     navController: NavHostController? = null,
     onMedClick: (Medication) -> Unit = {}
 ){
     val context = LocalContext.current
+    //    Log.d("MedicationState", medicationState.toString())
+
+    val medications = medicationState.medication
+    // for prototyping
+    //first row mes
+    val firstRowMeds = medications.take(3)
+    //second row mes
+    val secondRowMeds = medications.drop(3).take(3)
+
+    Log.d("MedicationState", firstRowMeds.toString())
+    Log.d("MedicationState", secondRowMeds.toString())
 
    val containerGradient = Brush.verticalGradient(
        colors = listOf(
-           Color(0xFFCCE7FF),
-           Color(0xFFBEE1FF),
-           Color(0xFFCCE7FF),
-           Color(0xFFD6ECFF),
+           Color(0xFF89C8FF),
+           Color(0xFFA0C8FD),
+           Color(0xFFCEE5FA),
+           Color(0xFFDCEDFC),
 
        )
    )
@@ -68,26 +68,12 @@ fun MedSection(
 
 
        ){
-//        Image(
-//            painter = painterResource(R.drawable.med_wave),
-//            contentDescription = null,
-//            contentScale = ContentScale.Crop,
-//            modifier = Modifier
-//                .matchParentSize()
-//                .padding(horizontal = 13.dp)
-//                .clip(RoundedCornerShape(16.dp)),
-//
-//        )
 
            Card (
                modifier = Modifier
                    .fillMaxWidth()
 
                    .align(Alignment.TopCenter)
-//            .paint(
-//                painterResource(R.drawable.med_wave),
-//                contentScale = ContentScale.FillBounds
-//            )
                    .border(1.dp, Color.LightGray, RoundedCornerShape(16.dp)),
                shape = RoundedCornerShape(16.dp),
                colors = CardDefaults.cardColors(
@@ -104,21 +90,14 @@ fun MedSection(
                        .padding(top = 13.dp),
                    horizontalArrangement = Arrangement.SpaceEvenly
                ) {
-                   val sideEffectsJson = Uri.encode(Gson().toJson(sideEffects))
-                   items(3){
+                   //val sideEffectsJson = Uri.encode(Gson().toJson(sideEffects))
+                   items(firstRowMeds){ medication ->
 
                        MedCard(
                            context,
-                           Medication(
-                               "Every evening",
-                               1,
-                               "Diazepam",
-                               "Dev",
-                               731,
-                               "10 mg",
-                           ),
+                            medication,
                            {
-                               navController?.navigate("medicationPage/$sideEffectsJson")
+                              // navController?.navigate("medicationPage/$sideEffectsJson")
                            }
                        )
                    }
@@ -131,17 +110,10 @@ fun MedSection(
                    horizontalArrangement = Arrangement.SpaceEvenly
 
                ) {
-                   items(3){
+                   items(secondRowMeds){ medication ->
                        MedCard(
                            context,
-                           Medication(
-                               "Every evening",
-                               1,
-                               "Paracetamol",
-                               "Dev",
-                               731,
-                               "500 mg"
-                           )
+                           medication
                        )
                    }
                }
